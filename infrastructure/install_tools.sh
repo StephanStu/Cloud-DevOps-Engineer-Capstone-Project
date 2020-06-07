67582a6f2384365ed60f4a8a31aba83ed07c03cb
Stephan Studener <stephanstudener@Stephans-MBP.fritz.box>
2020-06-07 19:40:18 +0200

    fix: brew echo commands and seperation of jenkins and tools in makefile.

--- infrastructure/install_tools.sh
+++ infrastructure/install_tools.sh
@@ -1,7 +1,10 @@
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # Missing commands here...
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/ubuntu/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 # Install docker on ubuntu-linux
+chmod u+x install_docker.sh
 ./install_docker.sh
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
@@ -11,6 +14,9 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+# Install pip
+sudo apt update
+sudo apt install python-pip
 # Install dependencies of Brew
 brew install gcc
 # Install hadolint, see local hadolint install instructions:   https://github.com/hadolint/hadolint
