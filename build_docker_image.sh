f28d16886ab8173e60b7271d6f1fc3c15f723588
Stephan Studener <stephanstudener@Stephans-MBP.fritz.box>
2020-06-09 21:11:22 +0200

    fix: build and upload step redesigned for dickerhub

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,1 +1,13 @@
-docker build -t udacity_capstone_project_repository .
+#!/usr/bin/env bash
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=predictor .
+
+# Step 2:
+# List docker images
+docker image ls
+
+# Step 3 (if you want to run locally and catch the app at port 8000 of the host):
+# Run flask app
+#docker run -p 8000:80 predictor
--- upload_docker_image_to_ecr.sh
+++
@@ -1,3 +0,0 @@
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 793553224113.dkr.ecr.eu-central-1.amazonaws.com
-docker tag udacity_capstone_project_repository:latest 793553224113.dkr.ecr.eu-central-1.amazonaws.com/udacity_capstone_project_repository:latest
-docker push 793553224113.dkr.ecr.eu-central-1.amazonaws.com/udacity_capstone_project_repository:latest
