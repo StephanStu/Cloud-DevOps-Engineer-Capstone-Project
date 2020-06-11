3e0464911735bdca22f1a9d1ea4112a6a63745aa
Stephan Studener <stephanstudener@Stephans-MBP.fritz.box>
2020-06-11 22:36:21 +0200

    fix: url in script fixed.

--- test_prediction.sh
+++ test_prediction.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-URL=http://a9ba30d04ac2011eaba070279dd3f610-1553933759.eu-central-1.elb.amazonaws.com/
 PORT=8000
 echo "Port: $PORT"
 
@@ -25,4 +24,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://$URL:$PORT/predict
+     -X POST http://a9ba30d04ac2011eaba070279dd3f610-1553933759.eu-central-1.elb.amazonaws.com:$PORT/predict
